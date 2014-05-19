436f0f3a12a7773f185cccd123a6f08dfee49d9a
Andrew Borovin <a.borovin@crystals.ru>
2014-05-19 17:42:59 +0400

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+initDom [![Build Status](https://drone.io/github.com/borovin/initDom/status.png)](https://drone.io/github.com/borovin/initDom/latest)
+==========
+
+init AMD modules on DOM elements
---
+++ karma.main.js
@@ -0,0 +1,15 @@
+var specFiles = Object.keys(window.__karma__.files).filter(function (file) {
+    return /spec\.js$/.test(file) && file.indexOf('bower_components') < 0;
+});
+
+var specModules = specFiles.map(function(moduleUrl){
+    return moduleUrl
+        .replace(/\/base\//g, '')
+        .replace(/.js/g, '');
+});
+
+require({
+    baseUrl: '/base/'
+}, function(){
+    require(specModules, window.__karma__.start);
+});
